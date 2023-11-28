02eb7eb761f0e4cfe32bf80517d2330aafb65b5b
agargaro <devgargaro@gmail.com>
2023-11-28 03:03:27 +0100

    wip

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  build: {
+    target: 'esnext', // Ensure that you are targeting a version of ECMAScript that supports top-level await
+  },
+});
